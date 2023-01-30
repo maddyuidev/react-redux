b89a090c735ba655180f4361364fe4e6394e3fe9
Madhu babu <mbmadhurai@gmail.com>
2023-01-30 09:10:31 +0530

    reversed to initial commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./components/Home";
-import Footer from "./components/layout/Footer";
-import Header from "./components/layout/Header";
-import ProductDetails from "./components/ProductDetails";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      {/* <Home /> */}
-
-      <div className="container container-fluid">
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/product/:id" element={<ProductDetails />} />
-          </Routes>
-        </Router>
-      </div>
-      <Footer />
-    </div>
-  );
+  return <div className="App">Hi</div>;
 }
 
 export default App;
